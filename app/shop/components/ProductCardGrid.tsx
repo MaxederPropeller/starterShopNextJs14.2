afb049d8d71388218000a0d3ff91ea9d6f76f5c1
maxederpropeller <max.schweller@gmail.com>
2024-05-23 09:25:27 +0200

    v7

--- app/shop/components/ProductCardGrid.tsx
+++ app/shop/components/ProductCardGrid.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
 import ProductCard from './ProductCard';
-
 import { readAllProducts } from '../helpers/firebase/actionFirebase';
 import { ProductType } from '../helpers/types';
 
-
-
-
-
-export default async function ProductCardGrid () {
+export default async function ProductCardGrid() {
   const products = await readAllProducts() as ProductType[];
 
   return (
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-4 h-full">
-    {products.map((product) => (
-      <ProductCard key={product.id} product={product} />
-    ))}
-  </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
   );
-};
-
-
+}
